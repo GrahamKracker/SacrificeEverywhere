6b8c381a3c94d1c577dcd083c9297575fa0c0b5c
GrahamKracker <gjguzynski@gmail.com>
2024-08-13 03:04:25 -0400

    update version

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -4,8 +4,8 @@ public static class ModHelperData
 {
     public const string Name = "SacrificeEverywhere";
     public const string Description = "SacrificeEverywhere by GrahamKracker";
-    public const string Version = "1.0.0";
+    public const string Version = "1.0.1";
     public const string RepoOwner = "GrahamKracker";
     public const string RepoName = "SacrificeEverywhere";
-    public const string WorksOnVersion = "34.3";
+    public const string WorksOnVersion = "44.1";
 }
